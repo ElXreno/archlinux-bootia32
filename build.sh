0c68cc5b6119ed2e1e5d9fcec80505b74a823f99
ElXreno <40758597+ElXreno@users.noreply.github.com>
2019-01-25 17:26:05 +0300

    Autobuild with Travis-CI (#1)
    * Travis-ci init
    
    * Travis-ci init
    
    * Okay. Testing it.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+FSLABEL='ARCH_201901'
+
+echo 'mkdir'
+
+mkdir out
+
+echo 'sed'
+
+sed -i -e "s/<FS-LABEL>/${FSLABEL}/g" grub.cfg
+
+echo 'make'
+
+grub-mkstandalone -d /usr/lib/grub/i386-efi/ -O i386-efi --modules="part_gpt part_msdos" --fonts="unicode" --locales="uk" --themes="" -o "out/bootia32.efi" "boot/grub/grub.cfg=grub.cfg" -v
