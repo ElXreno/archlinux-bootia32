4b8cb8f559a3abfd7a650d985fe6e2e067406ff8
ElXreno <elxreno@gmail.com>
2021-05-31 22:17:57 +0300

    Replace Travis CI by Github Actions
    Change-Id: I26e10ba6ccad63f8c3c77391a538c699fb215956

--- build.sh
+++ build.sh
@@ -12,4 +12,4 @@ echo 'sed'
 sed -i -e "s/<FS-LABEL>/${FSLABEL}/g" grub.cfg
 
 echo 'make'
-grub-mkstandalone -d /usr/lib/grub/i386-efi/ -O i386-efi --modules="part_gpt part_msdos" --fonts="unicode" --locales="uk" --themes="" -o "out/bootia32.efi" "boot/grub/grub.cfg=grub.cfg" -v
+grub-mkstandalone -d /usr/lib/grub/i386-efi/ -O i386-efi --modules="part_gpt part_msdos" --fonts="unicode" --locales="en@cyrillic" --themes="" -o "out/bootia32.efi" "boot/grub/grub.cfg=grub.cfg" -v
